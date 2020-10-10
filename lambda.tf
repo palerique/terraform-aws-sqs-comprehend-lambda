692127bc23c71e211936e25c53b6428dcef3b192
Paulo Henrique Lerbach Rodrigues <palerique@gmail.com>
2020-10-10 11:54:30 -0300

    Start adding language processor lambda

--- lambda.tf
+++ lambda.tf
@@ -1,3 +1,6 @@
+//************************************************
+//EVENT_PROCESSOR_LAMBDA:
+//************************************************
 data "archive_file" "eventProcessorLambdaFile" {
   type = "zip"
   source_dir = "${path.module}/eventProcessorLambda"
@@ -13,13 +16,42 @@ resource "aws_lambda_function" "eventProcessorLambda" {
   filename = data.archive_file.eventProcessorLambdaFile.output_path
   source_code_hash = data.archive_file.eventProcessorLambdaFile.output_base64sha256
 
-  timeout = 30
+  timeout = 3
   memory_size = 128
 
   environment {
     variables = {
       ROLE_ARN = aws_iam_role.terraform-aws-sqs-comprehend-lambda.arn
-      S3_ADDRESS = aws_s3_bucket.terraform-aws-sqs-comprehend-lambda-s3-bucket.arn
+    }
+  }
+}
+
+//************************************************
+//LANGUAGE_PROCESSOR_LAMBDA:
+//************************************************
+data "archive_file" "languageProcessorLambdaFile" {
+  type = "zip"
+  source_dir = "${path.module}/languageProcessorLambda"
+  output_path = "${path.module}/languageProcessorLambda.js.zip"
+}
+
+
+resource "aws_lambda_function" "languageProcessorLambda" {
+  function_name = "languageProcessorLambda"
+  handler = "languageProcessorLambda.handler"
+  role = aws_iam_role.terraform-aws-sqs-comprehend-lambda.arn
+  runtime = "nodejs12.x"
+
+
+  filename = data.archive_file.languageProcessorLambdaFile.output_path
+  source_code_hash = data.archive_file.languageProcessorLambdaFile.output_base64sha256
+
+  timeout = 3
+  memory_size = 128
+
+  environment {
+    variables = {
+      ROLE_ARN = aws_iam_role.terraform-aws-sqs-comprehend-lambda.arn
     }
   }
 }
